6255ff82b9bbde2e273505f4e2c07c8e4ad38f7b
Damian Sosnowski <sosnowski@outlook.com>
2014-12-08 21:25:14 +0100

    Remove stack trace

--- Lesson 1/code/lib/jasmine-2.0.0/jasmine-html.js
+++ Lesson 1/code/lib/jasmine-2.0.0/jasmine-html.js
@@ -121,7 +121,7 @@ jasmineRequire.HtmlReporter = function(j$) {
         for (var i = 0; i < result.failedExpectations.length; i++) {
           var expectation = result.failedExpectations[i];
           // messages.appendChild(createDom("div", {className: "result-message"}, expectation.message));
-          messages.appendChild(createDom("div", {className: "stack-trace"}, expectation.stack));
+          // messages.appendChild(createDom("div", {className: "stack-trace"}, expectation.stack));
         }
 
         failures.push(failure);
