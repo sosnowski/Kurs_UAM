3d39c2aea07dd04ccfab5f92361a01eea12c3a82
andrzej <andrzej.matlosz@gmail.com>
2015-02-23 23:07:55 +0100

    Add exercises into lesson1 and move quiz to separate folder

--- Lesson 1/code/quiz/quiz.js
+++ Lesson 1/code/quiz/quiz.js
@@ -2,17 +2,17 @@
 
 /* Quiz: Fill the method call with the proper value. See the example in the first question */
 describe('Guess the value of the variable', function () {
-
+    
 	it('Question 1', function () {
 		var a;
 
-		expect(a).toBe(undefined);
+		expect(a).toBe(undefined); //!!!!!!!!!!!!!!!!!
 	});
 
 	it('Question 2', function () {
 		var a = 1, b = "2";
 
-		expect(a + b).toBe("12");
+		expect(a + b).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 
 	it('Question 3', function () {
@@ -21,12 +21,13 @@ describe('Guess the value of the variable', function () {
 		if (a) {
 			b = 3;
 		}
-		expect(b).toBe(1);
+        
+		expect(b).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 
 	it('Question 4', function () {
-		expect(typeof null).toBe("object");
-		expect(typeof 3).toBe("number");
+		expect(typeof null).toBe(); //!!!!!!!!!!!!!!!!!
+		expect(typeof 3).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 
 	it('Question 5', function () {
@@ -34,18 +35,18 @@ describe('Guess the value of the variable', function () {
 			var a = 1;
 		}
 
-		expect(a).toBe(1);
+		expect(a).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 
 	it('Question 6', function () {
 		var arr = [1, 2, 3, undefined, undefined];
-		expect(arr.length).toBe(5);
+		expect(arr.length).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 
 	it('Question 7', function () {
 		var arr = [1];
 		arr[99] = 2;
-		expect(arr.length).toBe(100);
+		expect(arr.length).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 
 
@@ -54,7 +55,7 @@ describe('Guess the value of the variable', function () {
 
 		obj[a] = 12;
 
-		expect(Object.keys(obj)[0]).toBe("undefined");
+		expect(Object.keys(obj)[0]).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 
 	it('Question 9', function () {
@@ -62,18 +63,19 @@ describe('Guess the value of the variable', function () {
 
 		var bar = function () {
 			var foo;
-			expect(foo).toBe(undefined);
+            
+			expect(foo).toBe(); //!!!!!!!!!!!!!!!!!
 
 			foo = 3;
 			b = 4;
 			return foo + 2;
 		};
 
-		expect(foo).toBe(1);
+		expect(foo).toBe(); //!!!!!!!!!!!!!!!!!
 		bar = bar();
 
-		expect(b).toBe(4);
-		expect(bar).toBe(5);
+		expect(b).toBe(); //!!!!!!!!!!!!!!!!!
+		expect(bar).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 
 	it('Question 10', function () {
@@ -83,19 +85,19 @@ describe('Guess the value of the variable', function () {
 		if (result === NaN) {
 			err = true;
 		}
-		expect(err).toBe(false);
+		expect(err).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 
 	it('Question 11', function () {
-		expect(false || true || false).toBe(true);
+		expect(false || true || false).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 
 	it('Question 12', function () {
-		expect(false || "" || null || "0" || "test" || NaN).toBe("0");
+		expect(false || "" || null || "0" || "test" || NaN).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 
 	it('Question 13', function () {
-		expect((true && ("t" + false) && {} && (1 - 1)) || ([undefined].length)).toBe(1);
+		expect((true && ("t" + false) && {} && (1 - 1)) || ([undefined].length)).toBe(); //!!!!!!!!!!!!!!!!!
 	});
 });
 /* jshint ignore:end */
